6b041e19267061bb5809fc36fda7476e27f8acad
Ramis Vakazov <vakazov@restream.rt.ru>
2020-04-14 19:19:17 +0300

    move state into redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
+import { Provider } from 'react-redux'
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from "./redux/store"
+
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <React.StrictMode>
+          <Router>
+              <App />
+          </Router>
+      </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
